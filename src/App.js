511a14c373bdad9aeebeed6074354799541364c5
sniirr <sniirr@gmail.com>
2021-07-18 14:48:45 +0300

    links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {Bridge} from "shared/dapp-bridge";
 import {Modals, showModal} from "shared/dapp-common/components/Modal";
 import ConnectModal from "shared/dapp-common/components/ConnectModal";
 import Notification from "shared/dapp-common/components/Notification"
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTelegramPlane, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 const modals = {
     'connect': <ConnectModal/>,
@@ -32,8 +34,8 @@ function App() {
                     <div className="info">
                         <h1>Bifrost Bridge</h1>
                         <p>
-                            Bifrost token bridge is developed by the DAPP Account DAO (DAD) and powered by the DAPP
-                            Network’s universal bridging technology.
+                            Bifrost token bridge is developed by the <a target="_blank" rel="noreferrer" href="https://daddydao.io/">DAPP Account DAO (DAD)</a> and powered by the <a target="_blank" rel="noreferrer" href="https://liquidapps.io/">DAPP
+                            Network</a>’s universal bridging technology.
                         </p>
                         <div className="list-item">
                             Login with both your EOS and Ethereum Wallets.
@@ -47,6 +49,14 @@ function App() {
                     />
                 </div>
             </div>
+            <footer>
+                <a href="https://t.me/DAD_Token" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faTelegramPlane}/>
+                </a>
+                <a href="https://github.com/DAPP-Account-DAO" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub}/>
+                </a>
+            </footer>
             <Modals modals={modals}/>
             <Notification/>
         </div>
